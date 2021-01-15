2849fabf91de98e3b7fcdc9800d072208ded63ed
Xavier P <xavierpdl@gmail.com>
2021-01-14 23:34:23 -0500

    Initial commit
    Created basic startpage.

---
+++ README.md
@@ -0,0 +1,17 @@
+# Startpage
+
+Obviously drawing inspiration from 
+[https://www.reddit.com/r/startpages/](r/startpages). Simple startpage but it's
+my startpage.
+
+There are a few things that I'd like to add/change...
+
+- Add a search bar
+- Background image (perhaps a few different ones)
+- Make the links section slightly transparent
+- Add some dynamic element/s. Possible ideas:
+    - Clock
+    - Weather
+    - Quotes
+
+Keep it simple, keep it clean.
---
+++ static/main.js
@@ -0,0 +1,123 @@
+const sources = [
+    { 
+        title: " general", 
+        links: [
+            {
+                title: "gmail", 
+                href: "https://www.gmail.com"
+            },
+            {
+                title: "youtube", 
+                href: "https://www.youtube.com/"
+            },
+            {
+                title: "drive", 
+                href: "https://www.drive.google.com/"
+            },
+            {
+                title: "canvas", 
+                href: "https://fiu.instructure.com"
+            },
+        ]
+    },
+    { 
+        title: " social", 
+        links: [
+            {
+                title: "whatsapp", 
+                href: "https://www.youtube.com/"
+            },
+            {
+                title: "r/piracy", 
+                href: "https://www.reddit.com/r/Piracy"
+            },
+            {
+                title: "r/vimporn", 
+                href: "https://www.reddit.com/r/vimporn/"
+            },
+            {
+                title: "r/unixporn", 
+                href: "https://www.reddit.com/r/unixporn"
+            },
+            {
+                title: "r/startpages", 
+                href: "https://www.reddit.com/r/startpages/"
+            },
+            {
+                title: "r/battlestations", 
+                href: "https://www.reddit.com/r/battlestations/"
+            },
+        ]
+    },
+    { 
+        title: " freedom", 
+        links: [
+            {
+                title: "1337x",
+                href: "https://1337x.to/"
+            },
+            {
+                title: "rarbg",
+                href: "https://rarbg.to/torrents.php"
+            },
+            {
+                title: "the-eye",
+                href: "https://the-eye.eu/"
+            },
+            {
+                title: "libgen",
+                href: "http://libgen.li/"
+            },
+            {
+                title: "zlib",
+                href: "https://z-lib.org/"
+            },
+        ]
+    },
+    { 
+        title: " tech", 
+        links: [
+            {
+                title: "github", 
+                href: "https://github.com/Xavier-PDL"
+            },
+            {
+                title: "nerdfonts",
+                href: "https://www.nerdfonts.com/cheat-sheet"
+            },
+            {
+                title: "guidedhacking", 
+                href: "https://guidedhacking.com/"
+            },
+            {
+                title: "bleepingcomputer",
+                href: "https://www.bleepingcomputer.com/"
+            }
+        ]
+    },
+];
+
+function populate() {
+    const linkContainers = document.getElementsByClassName("links-container");
+    for(let i = 0; i < linkContainers.length; i++) {
+        const element = linkContainers.item(i);
+        console.log(element);
+        const title = element.firstElementChild;
+        title.innerText = sources[i].title;
+        const links = element.lastElementChild.firstElementChild;
+        //const newItem = document.createElement('li');
+        const linksArray = sources[i].links;
+        for(let j = 0; j < linksArray.length; j++) {
+            const newItem = createLink( linksArray[j].title, 
+                                        linksArray[j].href );
+        links.appendChild(newItem);
+        }
+
+    }
+}
+
+function createLink(linkText, linkHref) {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = '<a href="' + linkHref + '">' + linkText + '</a>';
+    return listItem;
+}
